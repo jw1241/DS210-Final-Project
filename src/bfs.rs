965ede23a84f404d3edc7c60efa00a434ea2e28a
Jianhan Wang <jianhan@bu.edu>
2024-04-22 23:14:36 -0400

    Modified the compute min dist function to work and return all mins for all vertices and made a function to compute the mean of all the min dist

--- src/bfs.rs
+++ src/bfs.rs
@@ -1,39 +1,44 @@
-fn compute_min_distance(graph: &[Vec<usize>]) -> Vec<usize> {
-    let mut distance: Vec<Option<usize>> = vec![None; graph.len()];
-    let mut min_distance = Vec::new();
+use std::collections::{VecDeque, HashMap};
+
+pub fn compute_min_distance(graph: &Vec<Vec<usize>>) -> HashMap<usize, Vec<usize>> {
+    let mut distance_all: HashMap<usize, Vec<usize>> = HashMap::new();
     
-    for i in 0..graph.len()
-    let mut queue: VecDeque<usize> = VecDeque::new();
-    queue.push_back(start);
-    while let Some(v) = queue.pop_front() { // new unprocessed vertex
-        for &u in &graph[v] {
-            //if let None = distance[u] { // consider all unprocessed neighbors of v
-                //distance[u] = Some(distance[v].unwrap() + 1);
-                //queue.push_back(u);
-            //}
-        //}
-    //}
-    //for dist in distance.iter() {
-        //if let Some(d) = dist {
-            //min_distance.push(*d);
-        //} else {
-            //min_distance.push(0);
+    for i in 0..graph.len() {
+        let mut distance: Vec<Option<usize>> = vec![None; graph.len()];
+        let mut min_distance = Vec::new();
 
-        //}
-    //}
-    //return min_distance;
-//}
+        let mut queue: VecDeque<usize> = VecDeque::new();
+        queue.push_back(i);
+        distance[i] = Some(0);
 
-fn uniform(mut d: Vec<usize>) -> Vec<usize> {
-    let mut a = Vec::new();
-    for _ in 0..26111 {
-        let mut sum = 0;
-        for i in 0..262111 {
-            sum += d[i];
+        while let Some(v) = queue.pop_front() { // new unprocessed vertex
+            for &u in &graph[v] {
+                if let None = distance[u] { // consider all unprocessed neighbors of v
+                    distance[u] = Some(distance[v].unwrap() + 1);
+                    queue.push_back(u);
+                }
+            }
         }
-        a.push(sum);   
-        let b = d.split_off(26112);
-        d = b;
+
+        for dist in distance.iter() {
+            if let Some(d) = dist {
+                min_distance.push(*d);
+            } else {
+                min_distance.push(0);
+            }
+        }
+        distance_all.insert(i, min_distance);
     }
-return a;
+    return distance_all;
+}
+
+pub fn uniform(distance: &HashMap<usize, Vec<usize>>) -> HashMap<usize, f64> {
+    let mut mean_all: HashMap<usize, f64> = HashMap::new();
+
+    for (v, d) in distance.iter() {
+        let sum: usize = d.iter().sum();
+        let mean = sum as f64 / d.len() as f64;
+        mean_all.insert(*v, mean);
+    }
+    return mean_all;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+mod bfs;
 use std::fs::File;
 use std::io::prelude::*;
-use std::collections::VecDeque;
+use crate::bfs::{compute_min_distance, uniform};
 
-fn read_file(path: &str) -> (Vec<Vec<usize>>, usize) { //Read the file and returning a vector & # of vertices
+fn read_file(path: &str) -> Vec<Vec<usize>> { //Read the file and returning a vector & # of vertices
     let mut result: Vec<(usize, usize)> = Vec::new();
     let file = File::open(path).expect("Could not open file");
     let buf_reader = std::io::BufReader::new(file);
@@ -12,22 +13,22 @@ fn read_file(path: &str) -> (Vec<Vec<usize>>, usize) { //Read the file and retur
         let v: Vec<&str> = line.trim().split_whitespace().collect();
         let x = v[0].parse::<usize>().expect("Failed to get x");
         let y = v[1].parse::<usize>().expect("Failed to get y");
-        result.push((x, y));
         if x > max_index {
             max_index = x;
         }
+        result.push((x, y));
     }
+
     let mut graph: Vec<Vec<usize>> = vec![vec![];max_index + 1]; //Adjacency list
     for (i,j) in result.iter() {
         graph[*i].push(*j);
     }    
-    return (graph, max_index);
+    return graph;
 }
 
 fn main() {
-    let graph = read_file("amazon0302.txt");
-    //let a = compute_min_distance(2, &graph).len();
-    print!("{:?}", graph)
-    
+    let graph = read_file("pagerank_data.txt");
+    let a = uniform(&compute_min_distance(&graph));
+    print!("{:?}", a);
 }
 
